b13dbf66d2fa94858ba8f66d0d003c23e86899d7
Oskar Gewalli <gewalli@gmail.com>
2015-08-15 19:31:04 +0200

    Perch

---
+++ tests/EnumTests.fs
@@ -0,0 +1,17 @@
+﻿namespace Tests
+open NUnit.Framework
+open FsUnit
+open Perch
+open System.Text
+
+type CustomerGender = 
+    |Other=0
+    |Male=1
+    |Female=2
+
+[<TestFixture>] 
+type ``EnumTests``() =
+
+    [<Test>] member test.
+        ``CanParse`` ()=
+        (Enum.tryParse("Female") : CustomerGender option) |> should equal (Some( CustomerGender.Female))
---
+++ tests/Script.fsx
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "../lib/Enum.fs"
+#load "../lib/Option.fs"
+
+#r "System.Xml.Linq.dll"
+#load "../lib/Xml.fs"
+open Perch
+
+// Define your library scripting code here
+
